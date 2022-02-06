cef5abcd8553ed493a74c638005f42fbf80059c1
Seok chan YUN <seyun@e1r3p14.42.fr>
2022-02-06 19:06:56 +0100

    token split (ok)

--- srcs/parse.c
+++ srcs/parse.c
@@ -6,30 +6,30 @@
 /*   By: seyun <seyun@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 22:18:21 by seyun             #+#    #+#             */
-/*   Updated: 2022/02/06 18:26:41 by seyun            ###   ########.fr       */
+/*   Updated: 2022/02/06 19:06:37 by seyun            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-int		check_end_quote(char *line, int *i)
+int		check_end_quote(char *line, int i)
 {
-	if (line[*i] == '\"')
+	if (line[i] == '\"')
 	{
-		while (line[*i] != '\0')
+		while (line[i] != '\0')
 		{
-			if (line[*i] == '\"')
-				return (0);
-			(*i)++;
+			i++;
+			if (line[i] == '\"')
+				return (i);
 		}
 	}
-	else if (line[*i] == '\'')
+	else if (line[i] == '\'')
 	{
-		while (line[*i] != '\0')
+		while (line[i] != '\0')
 		{
-			if (line[*i] == '\'')
-				return (0);
-			(*i)++;
+			i++;
+			if (line[i] == '\'')
+				return (i);
 		}
 	}
 	return (-1);
@@ -39,18 +39,26 @@ int		counting_token(char *line)
 {
 	int i;
 	int count;
+	int quote_end;
 
 	i = 0;
+	quote_end = 0;
 	count = 0;
 	while (line[i] != '\0')
 	{	
+		printf("%c\n", line[i]);
 		if (ft_isspace(line[i]))
 			i++;
 		else if (line[i] == '\'' || line[i] == '\"')
 		{	
-			if (check_end_quote(line, &i))
+			quote_end = check_end_quote(line, i);
+			if (quote_end == -1)
 				return (-1);
+			else
+				i = quote_end;
+			i++;
 			count++;
+			printf("%d ------count inside \n", count);
 		}
 		else
 		{
@@ -58,10 +66,12 @@ int		counting_token(char *line)
 			{
 				if (line[i] == '\'' || line[i] == '\"')
 					break ;
+				printf("%c ----- inside while\n", line[i]);
+				i++;
 			}
 			count++;
+			printf("%d -----count inside\n", count);
 		}
-		i++;
 	}
 	return (count);
 }
@@ -77,13 +87,14 @@ int		tokenizer(char *line, t_token_info *token_info)
 	if (count == -1)
 	{	
 		printf("----end quote error\n");
-		exit(1);
-	}
-	token_info->token = (t_token *)malloc(sizeof(t_token) * count);
-	if (token_info->token == NULL)
 		return (-1);
+	}
+	//token_info->token = (t_token *)malloc(sizeof(t_token) * count);
+	//if (token_info->token == NULL)
+//		return (-1);
+	token_info = NULL;
 	printf("%d --- token count \n", count);
-	return (0);
+	return (count);
 }
 
 int		lexical_analyser(t_list *env, char *line, t_token_info *token_info)
@@ -92,6 +103,8 @@ int		lexical_analyser(t_list *env, char *line, t_token_info *token_info)
 
 	count = tokenizer(line, token_info);
 	printf("%d --- end count \n", count);
+	if (count == -1)
+		return (-1);
 	env = NULL;
 	return (0);
 }
@@ -109,12 +122,11 @@ void	parse(t_list *env, char *line)
 	while (env)
 	{
 		env_list = env->content;
-		printf("%s\n", env_list->value);
+		printf("%s\n", env_list->origin);
 		env = env->next;
 	}
-	//count = lexical_analyser(env, line, token_info);
-	printf("%s\n", line);
+	count = lexical_analyser(env, line, token_info);
 	if (count == -1)
-		exit(1); // error control 해줘야함.
+		printf("*************count -1***************\n");	
 	return ;
 }
